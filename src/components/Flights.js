fd34a23592a75ede8308a26120a241a6c6e7bb8c
Veronika Nicolaou <vernic@kth.se>
2020-02-06 11:53:20 +0100

    fixed search

--- src/components/Flights.js
+++ src/components/Flights.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 
-const Flights = ({flightQuotes}) => {
+ const Flights = (props) => {
 
     // let [responseFlights, setResponseFlights] = useState({});
     // let [error, setError] = useState(false);
@@ -23,19 +23,49 @@ const Flights = ({flightQuotes}) => {
     //     //    flightInfo = <b>Failed to load data, please try again</b>;
     //     //    break;
     // }
+    let quotesMinPrice = props.quotes.map(quotes =>
+        quotes["MinPrice"]);
+      let quoteId = props.quotes.map(quotes =>
+        quotes["QuoteId"]);
+      let outboundQuotes = props.quotes.map(quotes => 
+       quotes["OutboundLeg"]);
+      let destId = outboundQuotes.map(info => info["DestinationId"]);
+      let depDate = outboundQuotes.map(info => info["DepartureDate"]);
+      let originIddep = outboundQuotes.map(info => info["OriginId"]);
 
+      let inboundQuotes = props.quotes.map(quotes => 
+        quotes["InboundLeg"]);
+      let returndestId = inboundQuotes.map(info => info["DestinationId"]);
+      let originIdreturn = inboundQuotes.map(info => info["OriginId"]);
+      let returnDate = inboundQuotes.map(info => info["DepartureDate"]);
 
-  //  let flights = props.flightQuotes.map((flight, key) => 
-//        <p>Departure City: {flight.map(), key["QuoteId"]} ->  
-        {/* Destination ciity: {flight["Quotes"].flight["Places"].flight["Name"], key["Quotes"].key["OutboundLeg"].key["DestinationId"]} */}
-    //    </p>
-    //   );
-        return( 
+      let flightPlaces = props.places.map(info =>
+        (info["PlaceId"], info["IataCode"], info["Name"], info["CountryName"]));
+
+      let flightCarriers = props.carriers.map(info =>
+        info["Name"]);
+
+      
+        console.log(quotesMinPrice)
+        //let flights = props.responseFlights.map((flight, key) => 
+    return(
+       <div>
+          <ul> 
             <div>
-                <p>{JSON.stringify(flightQuotes)}</p>
-                <p>Hellooo</p>
+              <p>{quoteId}</p>
+              From
+              <p>{destId}{depDate}</p>
+              to
+              <p>{returndestId}{returnDate}{flightPlaces}</p>
+              {flightCarriers}
+              <p>Price: {quotesMinPrice}</p>
             </div>
-        );
+          </ul>
+        </div>
+
+     
+      );
+ 
     
 }
 
--- src/components/SearchFlights.js
+++ src/components/SearchFlights.js
@@ -6,78 +6,45 @@ import Passengers from "./Passengers";
 import React, {useState, useEffect} from 'react';
 import Flights from "./Flights";
 
+//  function SearchFlights(props){
+  const SearchFlights = (props) => {
 
- function SearchFlights(props){
 
+  //let flight = null;
   //const history = useHistory();
   //let flightData = [];
-  let [responseFlights, setResponseFlights] = useState([]);
-
+  let [responseFlights, setResponseFlights] = useState({});
+  let [flightQuotes, setFlightQuotes] = useState([]);
+  let [flightPlaces, setFlightPlaces] = useState([]);
+  let [flightCarriers, setFlightCarriers] = useState([]);
+  let [showFlights, setShowFlights ] = useState(false);
     //history.push("/flights");
     // useEffect(() => {
         //}, []);
 
     function handleOnClick() {
+      setShowFlights(true);
       getResponseFlights();
+      if (responseFlights["Quotes"]) {
+        setFlightQuotes(responseFlights["Quotes"]);
+      }
+      if (responseFlights["Places"]) {
+        setFlightPlaces(responseFlights["Places"]);
+      }
+      if (responseFlights["Carriers"]) {
+        setFlightCarriers(responseFlights["Carriers"]);
+      }
     }
     
-    //console.log("responseFlights"+responseFlights)
     
     function getResponseFlights(){
       props.model.getAllFlights()
       .then(response => {
          setResponseFlights(response)
-         responseFlights = response;
-         flightMapping();
-        //  console.log(response["Quotes"].map(quotes => 
-        //   quotes["OutboundLeg"]))
-         //console.log("flightData"+ JSON.stringify(flightData["Quotes"]))
-         //return response;
        })
        .catch(err => {
            console.log(err);
        });;
-    }
-
-    function flightMapping(){
-      let quotesMinPrice = responseFlights["Quotes"].map(quotes =>
-        quotes["MinPrice"]);
-      let quoteId = responseFlights["Quotes"].map(quotes =>
-        quotes["QuoteId"]);
-      let outboundQuotes = responseFlights["Quotes"].map(quotes => 
-       quotes["OutboundLeg"]);
-      let destId = outboundQuotes.map(info => info["DestinationId"]);
-      let depDate = outboundQuotes.map(info => info["DepartureDate"]);
-      let originIddep = outboundQuotes.map(info => info["OriginId"]);
-
-      let inboundQuotes = responseFlights["Quotes"].map(quotes => 
-        quotes["InboundLeg"]);
-      let returndestId = inboundQuotes.map(info => info["DestinationId"]);
-      let originIdreturn = inboundQuotes.map(info => info["OriginId"]);
-      let returnDate = inboundQuotes.map(info => info["DepartureDate"]);
-
-      let places = responseFlights["Places"].map(info =>
-        (info["PlaceId"], info["IataCode"], info["Name"], info["CountryName"]));
-
-      let carriers = responseFlights["Carriers"].map(info =>
-        info["Name"]);
-
-        console.log(quotesMinPrice)
-      return (
-        <div>
-          <ul> 
-            <div>
-              <p>{quoteId}</p>
-              From
-              <p>{destId}{depDate}{places}</p>
-              to
-              <p>{returndestId}{returnDate}{places}</p>
-              {carriers}
-              <p>Price: {quotesMinPrice}</p>
-            </div>
-          </ul>
-        </div>
-      );
     }
 
   return (
@@ -91,7 +58,7 @@ import Flights from "./Flights";
       <Passengers model={props.model} />
      
       <button className="button" onClick={handleOnClick}> Search </button>
-     
+      {showFlights ? <Flights quotes={flightQuotes} places={flightPlaces} carriers = {flightCarriers}/> : null}
       {/* { <Flights  flightQuotes = {flightMapping} /> } */}
       {/* {<Flights flightQuotes = {flightData["Quotes"].map((quotes, key) => 
           {quotes["OutboundLeg"].map((info,key) => {info["DestinationId"], info["DepartureDate"], key["OriginId"]}), key["QuoteId"]}
